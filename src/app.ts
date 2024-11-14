dd106580a346b6bcfc92b855727347abf348746c
Tiffany Utvær Gasser <tiffany.linn.utvaer.gasser@sparebank1.no>
2024-11-14 15:05:59 +0100

    Set dev mode, add basic router

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,55 @@
-import express, { Express, NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
-var createError = require("http-errors");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+//import express, { Express, NextFunction, Request, Response } from "express";
+const express = require("express");
+// var createError = require("http-errors");
+// var path = require("path");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+// var indexRouter = require("./routes/index");
+// var usersRouter = require("./routes/users");
 
-var app = express();
+//var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// // view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req: Request, res: Response, next: NextFunction) {
-  next(createError(404));
+// // catch 404 and forward to error handler
+// app.use(function (req: Request, res: Response, next: NextFunction) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+//module.exports = app;
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello word");
 });
 
-// error handler
-app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-
-module.exports = app;
